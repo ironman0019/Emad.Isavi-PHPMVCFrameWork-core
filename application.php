39f12324703ec7054dc07dbe6f878351c43c7e63
ironman0019 <emadesavi@gmail.com>
2024-02-27 03:47:49 +0330

    Add event on application.php

--- application.php
+++ application.php
@@ -4,6 +4,11 @@ namespace emadisavi\phpmvc;
 
 class Application {
 
+    const EVENT_BEFORE_REQUEST = 'beforeRequest';
+    const EVENT_AFTER_REQUEST = 'afterRequest';
+
+    protected array $eventListeners = [];
+
     public static string $ROOT_DIR;
     public string $userClass;
     public Router $router;
@@ -75,12 +80,26 @@ class Application {
 
     public function run()
     {
+        $this->triggerEvent(self::EVENT_BEFORE_REQUEST);
         try {
             echo $this->router->resolve();
         } catch (\Exception $e) {
             $this->response->setStatusCode($e->getCode());
             echo $this->view->renderView('_error',['exception' => $e]);
         }
+    }
+
+    public function triggerEvent($eventName)    
+    {
+        $callbacks = $this->eventListeners[$eventName] ?? [];
+        foreach ($callbacks as $callback) {
+            call_user_func($callback);
+        }
+    }
+
+    public function on($eventName, $callback)
+    {
+        $this->eventListeners[$eventName][] = $callback;
     }
 
 
